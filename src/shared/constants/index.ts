5a5398f6894c7c1365fc8de1ec8bf6851ae14949
ddtienptit <140362645+ddtienptit@users.noreply.github.com>
2023-12-21 17:02:14 +0700

    feat: integrate react-native-google-signin

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -7,3 +7,8 @@ export const SCREENS = {
   DETAIL: "Detail",
   SETTING: "Setting",
 };
+
+export const IOS_CLIENT_ID_GOOGLE =
+  "409582649419-j3ler20j0fklont9ivpeud6lmq7hkkqn.apps.googleusercontent.com";
+export const WEB_CLIENT_ID_GOOGLE =
+  "409582649419-0bojiri21udlkk2gov3fc8u4tr9i3dmm.apps.googleusercontent.com";
