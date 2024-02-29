f067c72e5b470b6eef32505ebc418a61457028ba
Quang Nhat <chuquangnhat0501@gmail.com>
2024-02-29 10:10:06 +0700

    .

--- src/screens/purchase-course/book-lesson/book.lesson.select.view.tsx
+++ src/screens/purchase-course/book-lesson/book.lesson.select.view.tsx
@@ -38,11 +38,6 @@ const BookLessonSelectView: React.FC<BookLessonSelectViewProps> = () => {
 
   React.useEffect(() => {
     getTimeAvailable({ course_id: courseId }).then((res) => {
-<<<<<<< HEAD
-=======
-      // setLoading(false);
->>>>>>> 6c0b392 (feat: add screen new home)
-      console.log("resresres", res);
       if (!res.isError) {
         setData(res.data);
       }
